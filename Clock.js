f31b5dcd577e9c6068720ad22b4988597a7d56e5
Noel Pena <127697218+noel-pena@users.noreply.github.com>
2023-10-25 22:06:46 -0400

    Add files via upload

---
+++ Clock.js
@@ -0,0 +1,15 @@
+function clockGreeting(hour){
+
+if (hour >= 6 && hour < 12)
+    {console.log('good morning');
+    }
+    
+else if (hour >= 12 && hour < 18)
+    {console.log('good afternoon');    
+    }
+         
+else console.log('good evening');
+}
+
+
+clockGreeting(10);
